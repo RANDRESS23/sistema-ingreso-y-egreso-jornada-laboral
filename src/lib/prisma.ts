ee7adf3fe4753f24c6d084f5312bd192c3230fbe
Raúl Quimbaya <raulquimbaya4@gmail.com>
2026-02-05 16:13:05 -0500

    fix: change small details to prisma file

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,10 @@
 import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from '@/generated/prisma/client'
-
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient
-}
+import { PrismaClient } from "../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaBetterSqlite3({ url: connectionString });
-
-const prisma = globalForPrisma.prisma || new PrismaClient({
-  adapter,
-})
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+const prisma = new PrismaClient({ adapter });
 
 export { prisma };
